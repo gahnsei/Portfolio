4c87baae15c1cc6ad542c5f860d0aed3dd93b06a
Sei Gahn <gahnseiwebdev@gmail.com>
2022-05-10 17:45:29 -0500

    feat POR-13/Skills: All skills are displayed on skills section.

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -3,7 +3,59 @@ import React from "react";
 function Skills() {
   return (
     <div className="section">
-      <div className="mySection Skills">Skills</div>
+      <div className="mySection Skills">
+        <span className="section-label">Skills</span>
+        <div className="skill-container">
+          <div className="skill-card">
+            <i class="devicon-react-original"></i>
+            <span>ReactJS</span>
+          </div>
+          <div className="skill-card">
+            <i className="fa-brands fa-js-square"></i>
+            <span>JavaScript</span>
+          </div>
+          <div className="skill-card">
+            <i class="fa-brands fa-html5"></i>
+            <span>HTML5</span>
+          </div>
+          <div className="skill-card">
+            <i class="fa-brands fa-css3"></i>
+            <span>CSS3</span>
+          </div>
+          <div className="skill-card">
+            <i class="fa-brands fa-sass"></i>
+            <span>Sass</span>
+          </div>
+          <div className="skill-card">
+            <i class="fa-solid fa-laptop-code"></i>
+            <span>SQL</span>
+          </div>
+          <div className="skill-card">
+            <i class="fa-brands fa-node-js"></i>
+            <span>NodeJS</span>
+          </div>
+          <div className="skill-card">
+            <i class="devicon-express-original"></i>
+            <span>ExpressJS</span>
+          </div>
+          <div className="skill-card">
+            <i class="devicon-postgresql-plain"></i>
+            <span>PostgreSQL</span>
+          </div>
+          <div className="skill-card">
+            <i class="devicon-vscode-plain"></i>
+            <span>VSCode</span>
+          </div>
+          <div className="skill-card">
+            <i class="fa-brands fa-git-alt"></i>
+            <span>Git</span>
+          </div>
+          <div className="skill-card">
+            <i class="devicon-heroku-plain"></i>
+            <span>Heroku</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
