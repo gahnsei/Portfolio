a641faa4ff5fcfef4a7fff2f6793d4cfb2efd186
Sei Gahn <gahnseiwebdev@gmail.com>
2022-05-10 18:57:46 -0500

    feat POR-24/Contact Info: Contact info is displayed on Contact section.

--- src/Components/ContactMe.jsx
+++ src/Components/ContactMe.jsx
@@ -3,7 +3,20 @@ import React from "react";
 function ContactMe() {
   return (
     <div className="section">
-      <div className="mySection ContactMe">Contact Me</div>
+      <div className="mySection ContactMe">
+        <div className="section-div">
+          <h2 className="section-label">Contact Me</h2>
+          <h3>You Got A Problem That Needs Solving?</h3>
+          <p>
+            “Sometimes it pays to stay in bed on Monday, rather than spending
+            the rest of the week debugging Monday’s code.” – Dan Salomon
+          </p>
+          <div className="contact-email">
+            <i className="fa-solid fa-envelope"></i>
+            <span>gahnseiwebdev@gmail.com</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
