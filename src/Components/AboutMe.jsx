5edc0860eddb66fa88e9c702051610b43abbd52b
Sei Gahn <gahnseiwebdev@gmail.com>
2022-05-09 22:34:54 -0500

    About me

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import myPicture from "../kob9325.jpg";
 
 function AboutMe() {
   return (
     <div className="section">
       <div className="mySection AboutMe">
-        <a href={`#s3`}>About Me</a>
+        <div className="about-div">
+          <span className="about-job">Software Developer</span>
+          <img className="about-picture" src={myPicture} alt="Sei Gahn" />
+        </div>
+        <div className="about-div">
+          <span className="about-job">About Me</span>
+          <p className="about-description">
+            What started off as a childhood interest has now grown into a full
+            blown profession. Software Development is the driving force for the
+            future of technology and being a part of this history brings me
+            great joy. The joys of bringing ideas to life was an experience felt
+            every day as I learned the languages of computers. In order to truly
+            feel like a force that drove the wheels of progress, gaining an
+            understanding of all aspects of Software Development was the bare
+            minimum. From front-end development, UX, Design, back-end
+            development, and databases; I gained a full understanding of what it
+            takes to become a full-stack Developer. As a product of Devmountain,
+            a development bootcamp based in Utah, becoming a full-stack
+            developer was just the beginning of my journey. I look forward to
+            experiencing the next part of that journey with you; whoever and
+            wherever you are.
+          </p>
+        </div>
       </div>
     </div>
   );
