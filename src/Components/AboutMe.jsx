4ce044b51a62cd72ac21615300689d4471d14ed2
Sei Gahn <gahnseiwebdev@gmail.com>
2022-05-09 20:51:20 -0500

    feat POR-19/Picture: Picture of me is displayed on About section.

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import myPicture from "../kob9325.jpg";
 
 function AboutMe() {
   return (
     <div className="section">
       <div className="mySection AboutMe">
-        <span className="about-job">Software Developer</span>
+        <div className="about-name-pic">
+          <span className="about-job">Software Developer</span>
+          <img className="about-picture" src={myPicture} alt="Sei Gahn" />
+        </div>
       </div>
     </div>
   );
