46548c7fa39553eda19ba046fe6a1aadb03e09f1
Sei Gahn <gahnseiwebdev@gmail.com>
2022-05-10 20:42:37 -0500

    feat POR-25/Form Input: Inputs are displayed on Contact section.

--- src/Components/ContactMe.jsx
+++ src/Components/ContactMe.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import useForm from "../Utils/UseForm";
 
 function ContactMe() {
+  const { handleFormSubmit, formError, contactForm, handleFormChange } =
+    useForm();
   return (
     <div className="section">
       <div className="mySection ContactMe">
@@ -16,6 +19,48 @@ function ContactMe() {
             <span>gahnseiwebdev@gmail.com</span>
           </div>
         </div>
+        <form className="section-div" onSubmit={handleFormSubmit}>
+          <div className="input-div">
+            <label className="email-label" for="name">
+              NAME
+              {formError.name && <span>Name Cannot Be Empty</span>}
+            </label>
+            <input
+              id="name"
+              type="text"
+              name="name"
+              value={contactForm.name}
+              onChange={handleFormChange}
+            />
+          </div>
+          <div className="input-div">
+            <label className="email-label" for="email">
+              EMAIL
+              {formError.email && <span>Email Cannot Be Empty</span>}
+            </label>
+            <input
+              id="email"
+              type="text"
+              name="email"
+              value={contactForm.email}
+              onChange={handleFormChange}
+            />
+          </div>
+          <div className="input-div">
+            <label className="email-label">
+              MESSAGE
+              {formError.message && <span>Message Cannot Be Empty</span>}
+            </label>
+            <textarea
+              type="text"
+              className="email-message"
+              name="message"
+              value={contactForm.message}
+              onChange={handleFormChange}
+            />
+          </div>
+          <button>Hit Me Up</button>
+        </form>
       </div>
     </div>
   );
