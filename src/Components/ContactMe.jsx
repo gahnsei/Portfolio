41c3ac3376fd2a892ce5a9f929aa73a92ac669e1
Sei Gahn <gahnseiwebdev@gmail.com>
2022-05-14 02:17:17 -0500

    feat POR-32: Responsive Contact section

--- src/Components/ContactMe.jsx
+++ src/Components/ContactMe.jsx
@@ -20,11 +20,13 @@ function ContactMe() {
       <div className="mySection ContactMe">
         <div className="section-div">
           <h2 className="section-label">Contact Me</h2>
-          <h3>You Got A Problem That Needs Solving?</h3>
-          <p>
-            “Sometimes it pays to stay in bed on Monday, rather than spending
-            the rest of the week debugging Monday’s code.” – Dan Salomon
-          </p>
+          <div className="contact-info">
+            <h3>You Got A Problem That Needs Solving?</h3>
+            <p>
+              “Sometimes it pays to stay in bed on Monday, rather than spending
+              the rest of the week debugging Monday’s code.” – Dan Salomon
+            </p>
+          </div>
           <div className="contact-email">
             <i className="fa-solid fa-envelope"></i>
             <span>gahnseiwebdev@gmail.com</span>
