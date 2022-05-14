fe732d9d152fbbc2b155faea549569cdc7f3703f
Sei Gahn <gahnseiwebdev@gmail.com>
2022-05-14 01:38:43 -0500

    feat POR-31: Responsive Skills section

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import NavBar from "./Components/NavBar";
 import FullPage from "./Components/FullPage";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,7 +13,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
